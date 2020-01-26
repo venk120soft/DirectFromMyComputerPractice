56f992ddff3ae9d646c902a00470077a88cecb0b
Venkatesh Ch <venk120soft@gmail.com>
2020-01-26 08:40:04 -0800

    Finding the Square root of the number
    Finding the Square root of the number by increment the i to 0.01 for more accurate increase the precision.

---
+++ ProblemSolving/FindSquareRoot.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemSolving
+{
+    class FindSquareRoot
+    {
+        public double FindSquareRootValue(float number)
+        {
+            // can also done by this.
+            Console.WriteLine(Math.Sqrt(number));
+            // this can be 0.001 for more accurate we can go more deep
+            for (double i = 0; i < number; i = i + 0.01)
+            {
+                if (i * i >= number)
+                {
+                    Console.WriteLine(i);
+                    return i;
+                }
+            }
+            return 0.00;
+        }
+    }
+}
