2a32c7fddc732ee81b614eb7d4bc4ff80a97e6e9
Venkatesh Ch <venk120soft@gmail.com>
2021-12-20 08:44:10 -0800

    Update RunningSum1D.cs

--- ProblemSolving/RunningSum1D.cs
+++ ProblemSolving/RunningSum1D.cs
@@ -9,9 +9,27 @@ namespace ProblemSolving
     /// <summary>
     ///  Running Sum of 1d Array
     ///  https://leetcode.com/problems/running-sum-of-1d-array/
+    /// Example 1:
+    ///         Input: nums = [1,2,3,4]
+    ///         Output: [1,3,6,10]
+    ///         Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+    /// Example 2:
+    ///     Input: nums = [1,1,1,1,1]
+    ///     Output: [1,2,3,4,5]
+    ///     Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+    /// Example 3:
+    ///     Input: nums = [3,1,2,10,1]
+    ///     Output: [3,4,6,16,17]
     /// </summary>
     class RunningSum1D
     {
+        public int[] RunningSum0(int[] nums) {
+             for(int i = 1; i < nums.Length ; i++){
+                 nums[i] = nums[i-1] + nums[i];
+             }
+             return nums;
+        }
+        
         /// <summary>
         /// 
         /// </summary>
